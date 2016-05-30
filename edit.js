1243b8bb9d919aece626ee937f3a6a2b8839cd0b
Giulio Seregni <giulio.seregni@pingendo.com>
2016-05-30 11:58:33 +0200

    more

---
+++ edit.js
@@ -0,0 +1,119 @@
+var tagsType = ["uno", "due"];
+
+var myApp = angular.module('catalogoApp',['ngTagsInput']);
+
+myApp.controller('taggerController', ['$scope', '$http', function($scope, $http) {
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAGY4E5salsmIBUmNdd6elKspWr2w_qZuE",
+    authDomain: "catalogo-3c65b.firebaseapp.com",
+    databaseURL: "https://catalogo-3c65b.firebaseio.com",
+    storageBucket: "catalogo-3c65b.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var tagsRef = firebase.database().ref('/tags');
+
+  tagsRef.on('value', function(data) {
+    $scope.$apply(function() {
+      $scope.tags = data.val();
+    })
+  });
+
+  $scope.newOne = {};
+  $scope.newOne.folder = "";
+  $scope.newOne.notaPubblica = "";
+  $scope.newOne.notaPrivata = "";
+  $scope.newOne.tags = "";
+
+  $scope.addTag = function(tag,e ) {
+    $scope.newOne.tag = $scope.newOne.tag + tag;
+    e.preventDefault();
+  }
+
+  $scope.createTag = function(ref, newVal) {
+    setTimeout(function() {
+      firebase.database().ref(ref).push(newVal);
+    },1);
+  }
+
+  $scope.deleteTag = function(ref, newVal) {
+    setTimeout(function() {
+      firebase.database().ref(ref).push(newVal);
+    },1);
+  }
+
+  $scope.renameGroup = function(ref, newVal) {
+    setTimeout(function() {
+      firebase.database().ref(ref).set(newVal);
+    },1 );
+  }
+
+  $scope.save = function() {
+    alert("saved");
+    firebase.database().ref('/details').push($scope.newOne);
+    
+  }
+
+  $scope.toggleEditable = function() {
+    $scope.editable = !$scope.editable;
+  }
+}]);
+
+
+
+angular.module('catalogoApp').directive("contenteditable", function() {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        ngModel.$setViewValue(element.text());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+       $(element[0]).keydown(function(e) {
+          if(e.which === 13) {
+            e.preventDefault();
+            setTimeout(function() {
+              scope.$apply(function(){
+                  scope.$eval(attrs.ngEnter, {'event': e});
+              });
+            },1);
+            return false;
+          }
+        });
+
+      element.bind("keydown", function() {
+        scope.$apply(read);
+      });
+    }
+  };
+});
+
+
+
+
+angular.module('catalogoApp').directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+
+      $(element[0]).keydown(function(e) {
+
+        if(e.which === 13) {
+          e.preventDefault();
+          setTimeout(function() {
+            scope.$apply(function(){
+                scope.$eval(attrs.ngEnter, {'event': e});
+            });
+          },1);
+          return false;
+        }
+      });
+    };
+});
+
--- search.js
+++ search.js
@@ -1,3 +1,36 @@
+// var tagsType = ["uno", "due"];
+
+
+// var myApp = angular.module('catalogoApp',['ngTagsInput', 'firebase']);
+
+
+
+
+
+// myApp.controller('taggerController', ['$scope', '$http' , "$firebaseArray", function($scope, $http, $firebaseArray) {
+  
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyAGY4E5salsmIBUmNdd6elKspWr2w_qZuE",
+//     authDomain: "catalogo-3c65b.firebaseapp.com",
+//     databaseURL: "https://catalogo-3c65b.firebaseio.com",
+//     storageBucket: "catalogo-3c65b.appspot.com",
+//   };
+//   firebase.initializeApp(config);
+
+//   var tagsRef = firebase.database().ref('/tags');
+
+  
+  
+  
+
+// }]);
+
+
+
+
+
+
 function intersect(a, b) {
     var t;
     // if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
@@ -11,37 +44,45 @@ var myApp = angular.module('catalogoApp',['ngTagsInput']);
 
 myApp.controller('searchController', ['$scope', '$http', function($scope, $http) {
   
+  var config = {
+    apiKey: "AIzaSyAGY4E5salsmIBUmNdd6elKspWr2w_qZuE",
+    authDomain: "catalogo-3c65b.firebaseapp.com",
+    databaseURL: "https://catalogo-3c65b.firebaseio.com",
+    storageBucket: "catalogo-3c65b.appspot.com",
+  };
+  firebase.initializeApp(config);
 
-  $scope.searchTags = [];
-  $http({
-    method: 'GET',
-    url: 'trello.json'
-  }).then(function successCallback(response) {
-    console.log("response" , response);
-    $scope.model = response;
-    // make some fixes on the model coming from trello
-    $($scope.model.data.cards).each(function() {
-      var card = this;
+  $scope.cards = [];
 
-      if (card.idList != "56a0dcd62f77fc562720b136")
-        return;
-
-      card.tags = card.name.toLowerCase().split("-");
-
-      $(card.tags).each(function(index) {
-        card.tags[index] = card.tags[index].trim();
-        var desc = card.desc.split("**");
-        console.log(desc);
-        card.nota = desc[2];
-      });
+  var tagsRef = firebase.database().ref('/details').once("value", function(val) {
+    $scope.$apply(function() {
+      var v = val.val();
+      for (var k in v) {
+        $scope.cards.push(v[k]);
+      }
     });
+  });
 
-  }, function errorCallback(response) {
+  var tagsRef = firebase.database().ref('/tags').once("value", function(val) {
+    $scope.$apply(function() {
+      $scope.allTags = val.val();
+      $scope.tags = val.val();
+      console.log($scope.tags);
+    });
   });
 
 
-  $scope.$watchCollection('filteredCards', function() { 
+  $scope.searchTags = [];
+
+  $scope.$watchCollection('searchTags', function() { 
+    
     setTimeout(function() {
+      if ($scope.searchTags.length == 0) {
+        $scope.$apply(function() {
+          $scope.tags = $scope.allTags;
+        });
+        return;
+      }
 
       var tags = [];
       $($scope.filteredCards).each(function() {
@@ -54,9 +95,23 @@ myApp.controller('searchController', ['$scope', '$http', function($scope, $http)
         }
       });
 
-      $scope.$apply(function() {
-        $scope.additionalTags = tags;
-      })
+      $scope.tags = [];
+      for (var k in $scope.allTags) {
+        var g = $scope.allTags[k];
+        var group = {};
+        group.tags = [];
+        group.name = g.name;
+        $scope.tags[k] = group;
+        
+       
+        for (var x in g.tags) {
+          if (tags.indexOf(g.tags[x]) > -1) {
+            group.tags.push(g.tags[x]);
+          }
+        }
+      }
+      $scope.$digest();
+     
     },1);
   })
 
@@ -77,11 +132,12 @@ myApp.controller('searchController', ['$scope', '$http', function($scope, $http)
 
 
   $scope.filterCards = function (card) {
-    if (card.idList != "56a0dcd62f77fc562720b136")
-      return false;
+    // return true;
+    // if (card.idList != "56a0dcd62f77fc562720b136")
+    //   return false;
 
-    if (!$scope.searchTags || !card.tags)
-      return true;
+    // if (!$scope.searchTags || !card.tags)
+    //   return true;
 
     return (intersect( $scope.searchTags, card.tags).length == $scope.searchTags.length );
   };
